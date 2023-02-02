74931ecc4ca9be91d1a5c9f497dec284da6325ac
JJMorton <jjmorton1636@gmail.com>
2023-02-02 15:40:17 +0000

    Reduce particle_mean_variance memory usage

--- pymusic_igw/tasks/particle_mean_variance.py
+++ pymusic_igw/tasks/particle_mean_variance.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 class ParticleMeanVariance(AnalysisTask):
 
     def __init__(self):
-        super().__init__(f"particle_penetration")
+        super().__init__(f"particle_mean_variance")
 
     def compute(self):
         # Get all tracer files in dump directory
@@ -27,20 +27,48 @@ class ParticleMeanVariance(AnalysisTask):
             return
         files = sorted(dumps_dir.glob("*.tracers.h5"))
         dumps = [pmp.ParticleDumpFromFile(f) for f in files]
-        logger.info(f"{len(dumps)} particle dumps from t={dumps[0].time:E} to t={dumps[-1].time:E}")
+        num_dumps = len(dumps)
+        num_particles = len(dumps[0].dataframe())
+        logger.info(f"{num_dumps} particle dumps from t={dumps[0].time:E} to t={dumps[-1].time:E}")
 
-        particles = {}
-        for gid in dumps[0].dataframe().index:
-            particles[gid] = []
-        for i, dump in enumerate(dumps):
-            df = dump.dataframe()
-            logger.info(f"Analysing particle dump {i} ({len(df)} particles)")
-            for gid, series in df.iterrows():
-                particles[gid].append(series["x1"])
+        # Reduce memory usage by increasing number of chunks to use
+        num_chunks = 4
+        chunk_size = int(np.ceil(num_particles / num_chunks))
+        logger.info(f"Calculating means in {num_chunks} chunks, {chunk_size} particles in each")
 
-        init_radii = np.array([r[0] for r in particles.values()])
-        mean_radii = np.array([np.mean(r) for r in particles.values()])
-        var_radii = np.array([np.var(r) for r in particles.values()]) # sum[(r - mean)**2] / N
+        # These arrays are 1D of length num_particles
+        init_radii_tot = np.array([])
+        mean_radii_tot = np.array([])
+        var_radii_tot = np.array([])
+
+        # Calculate each chunk
+        for chunk in range(num_chunks):
+            # Calculate which particles to use
+            p_start = chunk * chunk_size
+            p_end = (chunk + 1) * chunk_size
+            if p_end >= num_particles:
+                p_end = num_particles - 1
+            logger.info(f"Chunk {chunk + 1}, {p_end - p_start} particles")
+            radii = np.zeros((num_dumps, p_end - p_start))
+
+            # Read the dumps
+            logger.info("Reading particle dumps...")
+            for i, dump in enumerate(dumps):
+                df = dump.dataframe()
+                # logger.info(f"Analysing particle dump {i} ({len(df)} particles)")
+                radii[i] = np.array(df.sort_index()["x1"][p_start:p_end])
+
+            # Calculate the means and variances
+            init_radii = radii[0]
+            logger.info("Calculating means...")
+            mean_radii = np.mean(radii, axis=0)
+            logger.info("Calculating variances...")
+            var_radii = np.mean(np.power(radii - mean_radii[np.newaxis, :], 2), axis=0) # sum[(r - mean)**2] / N
+
+            # Append to complete arrays
+            init_radii_tot = np.concatenate((init_radii_tot, init_radii))
+            mean_radii_tot = np.concatenate((mean_radii_tot, mean_radii))
+            var_radii_tot = np.concatenate((var_radii_tot, var_radii))
 
         return init_radii, mean_radii, var_radii
 
@@ -49,17 +77,26 @@ class ParticleMeanVariance(AnalysisTask):
         init_radii /= self.params.radius
         mean_radii /= self.params.radius
         var_radii /= self.params.radius**2
-        fig = plt.figure(figsize=(4 * 2, 3))
+        fig = plt.figure(figsize=(5 * 2, 3.5))
         ax_mean = fig.add_subplot(1, 2, 1)
         ax_var = fig.add_subplot(1, 2, 2)
-        ax_mean.scatter(init_radii, mean_radii, marker=',')
-        ax_var.scatter(init_radii, var_radii, marker=',')
-        ax_mean.set_xlabel("Initial radius (stellar radii)")
-        ax_mean.set_ylabel("Mean radius (stellar radii)")
-        ax_var.set_xlabel("Initial radius (stellar radii)")
-        ax_var.set_ylabel("Variance of radial position (stellar radii sq)")
+        ax_mean.scatter(init_radii, mean_radii, marker=',', alpha=0.05, lw=0, s=1)
+        ax_var.scatter(init_radii, np.sqrt(var_radii), marker=',', alpha=0.05, lw=0, s=1)
+        for ax in (ax_mean, ax_var):
+            ax.axvline(self.params.boundary_conv / self.params.radius,
+                ls="-", label=r"$r_{\rm conv}$", lw=1, c="black")
+            ax.axvline((self.params.l_max_heatflux + self.params.boundary_conv) / self.params.radius,
+                ls="--", label=r"$l_{\rm max}(\mathbf{f}_{\delta T})$", lw=1, c="black")
+            ax.axvline((self.params.l_max_ekinflux + self.params.boundary_conv) / self.params.radius,
+                ls="-.", label=r"$l_{\rm max}(\mathbf{f}_k)$", lw=1, c="black")
+            ax.legend()
+        ax_mean.set_xlabel("Initial radius")
+        ax_mean.set_ylabel("Mean radius")
+        ax_var.set_xlabel("Initial radius")
+        ax_var.set_ylabel("Std. dev. of radius")
         fig.tight_layout()
         return fig
+
 
 if __name__ == "__main__":
     ParticleMeanVariance().run()
